3c9d280ac99afe784c973802be4e1c106021f5ad
Lorenzo Jokhan <lorenzoluk@gmail.com>
2025-03-26 17:12:45 +0100

    Added github deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { IntroductionPage } from "./pages/introduction";
 import { Work } from "./pages/work";
