29b2af4ef2dc0baa89df7250ca388a65d8d28e31
Lorenzo Jokhan <lorenzoluk@gmail.com>
2025-03-26 21:01:10 +0100

    Added base option

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   build: {
     outDir: "./docs",
     emptyOutDir: true,
+    assetsDir: "assets",
   },
   plugins: [react(), tailwindcss()],
 });
